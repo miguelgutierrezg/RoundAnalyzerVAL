22e2460edb0f0bb63c9b0749e1eab9cc17247a14
miguelgutierrezg <miguelgugavilla@gmail.com>
2025-04-18 22:44:37 +0200

    Cambio en el titulo prueba

--- analizer/vite.config.js
+++ analizer/vite.config.js
@@ -1,8 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/RoundAnalyzerVAL/', 
   plugins: [react()],
+  base: '/RoundAnalyzerVAL/',
+  build: {
+    outDir: 'docs',
+  }
 })
